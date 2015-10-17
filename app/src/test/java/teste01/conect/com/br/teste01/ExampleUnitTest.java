2d1a808c0d8f6aa623f2e4175580ced71af2fbc3
conectsolutions@hotmail.com <conectsolutions@hotmail.com>
2015-10-17 15:22:56 -0300

    Initial commit

---
+++ app/src/test/java/teste01/conect/com/br/teste01/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package teste01.conect.com.br.teste01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
